1d1086e4a46cd23f7b35295d132be662102bf333
Łukasz <lbialobrzeski1@gmail.com>
2019-02-17 21:45:47 +0100

    Merge branch develop

---
+++ liczby_pierwsze/cmake-build-debug/CMakeFiles/liczby_pierwsze.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/liczby_pierwsze.dir/main.cpp.o"
+  "liczby_pierwsze.pdb"
+  "liczby_pierwsze"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/liczby_pierwsze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zadanie_probne/cmake-build-debug/CMakeFiles/Zadanie_probne.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zadanie_probne.dir/main.cpp.o"
+  "Zadanie_probne.pdb"
+  "Zadanie_probne"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zadanie_probne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
